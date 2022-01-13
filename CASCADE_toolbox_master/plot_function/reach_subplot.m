85817ae953fa47b09793379c24b06dd86f0a4d60
mtangi <marco.tangi@polimi.it>
2022-01-13 11:21:39 +0100

    Update reach_subplot.m

--- CASCADE_toolbox_master/plot_function/reach_subplot.m
+++ CASCADE_toolbox_master/plot_function/reach_subplot.m
@@ -11,6 +11,7 @@ node_ID = ReachData([ReachData.reach_id] == reach_ID).FromN;
 
 %% define cumulative sediment distribution and D_values
 
+
 dmi= 2.^(-psi)/1000;
 dmi_finer = dmi*1000;
 D_values = [16 50 84];
@@ -27,13 +28,22 @@ end
 %% find reach D values from GSD
 
 %find reach sediment size from ReachData
-D_changes = [ReachData(reach_ID).D16 ReachData(reach_ID).D50 ReachData(reach_ID).D84];
+%D_changes = [ReachData(reach_ID).D16 ReachData(reach_ID).D50 ReachData(reach_ID).D84];
+
+%compute changes in sediment size using linear interpolation of the sediment distribution
+D_changes = zeros(length(D_values),1);
+for j=1:length(D_values)
+   a = min( find( Perc_finer(:,node_ID) >  D_values(j), 1, 'last' ),length (psi)-1);
+   D_changes(j) = (D_values(j) - Perc_finer(a+1,node_ID))/(Perc_finer(a,node_ID) - Perc_finer(a+1,node_ID))*(dmi(a)-dmi(a+1))+dmi(a+1);
+   D_changes(j) = D_changes(j)*(D_changes(j)>0) + dmi(end)*(D_changes(j)<0);
+end
 
 %% plot figure
 
 fig = figure('rend','painters','pos',[100 150 1000 600]);
     
-    %SUBPLOT 1 : reach geomorphological features and sediment transport fluxes
+    %SUBPLOT 1 : reach geomorphological features and sediment transport
+    %fluxes
     
     ax = subplot(2, 2, 1);
     %choose method to display sediment flows
@@ -50,13 +60,14 @@ fig = figure('rend','painters','pos',[100 150 1000 600]);
 
     text(0,0.5,str,'FontSize',16);
     set ( ax, 'visible', 'off')
+    %annotation('textbox',[.57 .63  0.5 0.5],'String',str,'FitBoxToText','on', 'verticalalignment', 'bottom');
 
     %SUBPLOT 2 : upstream river network and sediment contribution to the
     %reach
     subplot(2,2,2)
     
     %define colorscale for sediment contribution to the reach (up_nodes)
-    up_nodes = find(Network.Distance_Upstream{node_ID,1} ~= Inf); % find up_nodes)
+    up_nodes = find(Network.Upstream.Distance{node_ID,1} ~= Inf); % find up_nodes)
     col = [ceil(  QB_tr(up_nodes, node_ID) ./  max(QB_tr(up_nodes , node_ID)) .*99+0.001);1];
     
     col(isnan(col)) = 100; %remove NaN
@@ -65,7 +76,7 @@ fig = figure('rend','painters','pos',[100 150 1000 600]);
     color_scale = hot(220);
     color_scale = [color_scale(1:5,:); color_scale(50:end,:)];
 
-    %plot full network - mantain river shape
+    %plot full network
     
 %     plot([ReachData(node).X],[ReachData(node).Y],'Color','blue','linewidth',6);
 %     hold on
@@ -77,7 +88,7 @@ fig = figure('rend','painters','pos',[100 150 1000 600]);
 %         
 %     end
     
-    %plot simplified network - straigth lines between nodes
+    %plot simplified network
     
     line([ReachData(node_ID).x_FN  ReachData(node_ID).x_TN],[ReachData(node_ID).y_FN  ReachData(node_ID).y_TN],...
         'color','blue','linewidth',10);
@@ -95,6 +106,7 @@ fig = figure('rend','painters','pos',[100 150 1000 600]);
     if max(QB_tr(up_nodes,node_ID))>10^4 
         legEnt=num2str(tick_val','%10.2E'); 
     else
+        
         legEnt=num2str(round(tick_val',4));
     end 
     
@@ -110,7 +122,8 @@ fig = figure('rend','painters','pos',[100 150 1000 600]);
     pax = gca;
     pax.FontSize = 10;
 
-    %SUBPLOT 3 : grain size distribution of the total sediment flux passing through the reach
+    %SUBPLOT 3 : grain size distribution of the total sediment flux
+    %passing through the reach
     subplot(2,2,3 )
 
     semilogx(dmi*1000,Perc_finer(:,node_ID),'LineWidth',2); 
@@ -128,13 +141,14 @@ fig = figure('rend','painters','pos',[100 150 1000 600]);
     str={[sprintf(' D%d = ',D_values(1)), num2str(D_changes(1)*1000), ' mm'] , [sprintf(' D%d = ',D_values(2)), num2str(D_changes(2)*1000),' mm'] , [sprintf(' D%d = ',D_values(3)), num2str(D_changes(3)*1000),' mm']};
     annotation('textbox',[.15 .25  0.5 0.5],'String',str,'FitBoxToText','on', 'verticalalignment', 'bottom');
 
-    %SUBPLOT 4 : deposition and entraining for each sediment class considered
+    %SUBPLOT 4 : deposition and entraining for each sediment class
+    %considered
     subplot(2,2,4 )
     b = bar([squeeze(Qbi_tr(node_ID,node_ID,:)),nansum(squeeze(Qbi_dep(:,node_ID,:)),1)']);
     b(1).FaceColor = 'r';b(2).FaceColor = 'g';
     ylabel('Kg/s');
     xlabel('sediment classes [phi]');
-    
+    %ylim([0,ylimit])
     legend({'Q_s_e_d entrained','Q_s_e_d deposited'},'Location','northeast')
     str=sprintf('Deposition and entraining for each sediment class');
     title(str)
@@ -144,4 +158,3 @@ fig = figure('rend','painters','pos',[100 150 1000 600]);
 
 
 end
-
